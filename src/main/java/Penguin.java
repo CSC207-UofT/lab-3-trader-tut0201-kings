517cec27022d10c1bd91dca355e22bc7e076c6cb
Baker Jackson <bakerwjackson@gmail.com>
2021-09-27 12:48:40 -0400

    Made penguin class more compact

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,1 +1,1 @@
-class Penguin implements Tradable,Domesticatable{@Override public int getPrice() {return 10000000;}@Override public String sound() {return "MEEP";} public String getName(){return "Rico";}}
+class Penguin implements Tradable,Domesticatable{@Override public int getPrice(){return 10000000;}@Override public String sound(){return "MEEP";}public String getName(){return "Rico";}}
