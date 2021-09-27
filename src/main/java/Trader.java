3e1e990e1f91c2e796aba323facfc3b2e6c57f68
Baker Jackson <bakerwjackson@gmail.com>
2021-09-27 12:43:04 -0400

    Add penguin class

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -96,7 +96,9 @@ public class Trader {
         List<Tradable> all_items = Arrays.asList(
                 new Horse(),
                 new Horse(),
-                new Horse()
+                new Horse(),
+                new Penguin(),
+                new Penguin()
                 // TODO: Add Tradable objects here!
         );
 
